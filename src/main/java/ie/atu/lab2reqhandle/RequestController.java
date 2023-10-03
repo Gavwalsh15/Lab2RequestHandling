7e334ed497cab24131ce477970e970c87706b58e
Gavin Walsh <G00402296@atu.ie>
2023-10-03 15:59:01 +0100

    Ex3 request Params

--- src/main/java/ie/atu/lab2reqhandle/RequestController.java
+++ src/main/java/ie/atu/lab2reqhandle/RequestController.java
@@ -3,6 +3,7 @@ package ie.atu.lab2reqhandle;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class RequestController {
@@ -15,5 +16,11 @@ public class RequestController {
     public String greetByName(@PathVariable String name){
 
         return "Hello, " + name + "!";
+    }
+
+    @GetMapping("/details")
+    public String details(@RequestParam String name, int age){
+        //http://localhost:8080/details?age=33&name=gavin
+        return "Hello, " + name + ", Aged " + age + "!";
     }
 }
