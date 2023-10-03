0371d3c43d2709d1b7bec1e4d5220925a604276d
Gavin Walsh <G00402296@atu.ie>
2023-10-03 15:30:03 +0100

    First Commit all files

---
+++ src/main/java/ie/atu/lab2reqhandle/Lab2ReqHandleApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab2reqhandle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2ReqHandleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2ReqHandleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab2reqhandle/Lab2ReqHandleApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab2reqhandle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2ReqHandleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
